199a6fd0798a4e280a27f01baded93a8007fbbe4
juliarubik <43538276+juliarubik@users.noreply.github.com>
2018-11-15 11:34:44 +0300

    Add files via upload

---
+++ spiiran/Assets/Scripts/BuildingPopup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingPopup : MonoBehaviour {
+
+    public void Open()
+    {
+        gameObject.SetActive(true);
+    }
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+}
